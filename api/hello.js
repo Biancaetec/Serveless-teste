ce0245eff03b90d058b7ab3ae573e4c5c84c2a9d
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-08-20 11:53:53 +0000

    commit

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-export default function handler(res,req) {
+export default function handler(req, res) {
     const nome = req.query.nome || "mundo";
     res.status(200).json({ mensagem: `Olá, ${nome}!` });
 }
