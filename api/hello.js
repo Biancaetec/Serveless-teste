20cfa16e4cf36c265082d96b099232cd4d54f37d
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-08-20 11:45:45 +0000

    [feat]arrumando

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 export default function handle (res,req) {
-    const nome = req.query.nome || 'Mundo';
+    const nome = req.query.nome || 'mundo';
     req.status(200).json({ message: `Olá, ${nome}!` });
 }
